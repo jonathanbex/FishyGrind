a2a5bdb5a7dd4175545bd14b199445ae8b969d0b
JonathanBex <jonathan.soderberg@hotmail.com>
2019-05-29 18:55:46 +0200

    cleanup

--- FishingBot/Methods/Fisher.cs
+++ FishingBot/Methods/Fisher.cs
@@ -19,14 +19,13 @@ namespace FishingBot.Methods
 {
   class Fisher
   {
+    //Windows Api
     [DllImport("user32.dll")]
     static extern bool GetCursorPos(ref Point lpPoint);
     [DllImport("User32.dll")]
     static extern int SetForegroundWindow(IntPtr point);
-
     [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
     public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-
     [DllImport("user32.dll", EntryPoint = "keybd_event", CharSet = CharSet.Auto, ExactSpelling = true)]
     public static extern void keybd_event(byte vk, byte scan, int flags, int extrainfo);
     [DllImport("User32.dll")]
@@ -36,56 +35,72 @@ namespace FishingBot.Methods
 
     private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
     private const int MOUSEEVENTF_RIGHTUP = 0x10;
-    Point cursor = new Point();
-    Rect windowRec = new Rect();
     private string bind;
     private int timer;
     private int lureTimer;
     private bool pause = false;
     private string rodBind;
-    Logger logger;
-    GrindFish mainWindow = null;
-    IntPtr h;
-    ColorHelpers colorHelpers;
     private DateTime EndTime;
     private DateTime LastAppendedLure;
     byte KEYBDEVENTF_SHIFTVIRTUAL = 0x10;
     byte KEYBDEVENTF_SHIFTSCANCODE = 0x2A;
     int KEYBDEVENTF_KEYDOWN = 0;
     int KEYBDEVENTF_KEYUP = 2;
+    IntPtr h;
+
+    Point cursor = new Point();
+    Rect windowRec = new Rect();
+
+    Logger logger;
+    GrindFish mainWindow = null;
+    ColorHelpers colorHelpers;
     Bitmap startPos = null;
+
     public Fisher() {
       this.colorHelpers = new ColorHelpers();
+
       var config = new NLog.Config.LoggingConfiguration();
 
       var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
 
       config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
-
+ 
       NLog.LogManager.Configuration = config;
+
       this.logger = NLog.LogManager.GetCurrentClassLogger();
     }
 
 
     public void Run(string bind,string rodBind, int timer,int lureTimer, GrindFish main)
     {
-      //focus process
-      var process = Process.GetProcessesByName("WOW").FirstOrDefault();
-      if (process == null) process = Process.GetProcessesByName("WowB").FirstOrDefault();
-      if (process == null) return;
-      WindowsIdentity user = WindowsIdentity.GetCurrent();
-      WindowsPrincipal principal = new WindowsPrincipal(user);
-      bool isadmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
-      process.WaitForInputIdle();
-      h = process.MainWindowHandle;
-      HotkeyManager.Current.AddOrReplace("Pause", Key.P, ModifierKeys.Control, HotKeyPressed);
-      SetForegroundWindow(h);
       this.bind = bind;
       this.timer = timer;
       this.mainWindow = main;
       this.rodBind = rodBind;
       this.EndTime = DateTime.Now.AddMinutes(timer);
       this.lureTimer = lureTimer;
+      //focus process
+      //vanilla
+      var process = Process.GetProcessesByName("WOW").FirstOrDefault();
+      //classic
+      if (process == null) process = Process.GetProcessesByName("WowB").FirstOrDefault();
+
+      if (process == null) return;
+
+      //legacy check if administrator, no need.
+      //WindowsIdentity user = WindowsIdentity.GetCurrent();
+      //WindowsPrincipal principal = new WindowsPrincipal(user);
+      //bool isadmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+  
+      process.WaitForInputIdle();
+      h = process.MainWindowHandle;
+
+      //global hotkey for pause
+      HotkeyManager.Current.AddOrReplace("Pause", Key.P, ModifierKeys.Control, HotKeyPressed);
+
+      SetForegroundWindow(h);
+
+
       //5 seconds to get cursor into position
       mainWindow.Invoke(new MethodInvoker(() => mainWindow.ChangeStatusText("Starting in", false)));
       Thread.Sleep(1000);
@@ -105,33 +120,9 @@ namespace FishingBot.Methods
 
     }
 
-    public Bitmap GetPicture(Point location)
-    {
-      Size size = new Size(200, 200);
-      Bitmap screenPixel = new Bitmap(200, 200, PixelFormat.Format32bppArgb);
-      using (Graphics gdest = Graphics.FromImage(screenPixel))
-      {
-        location.X -= 75;
-        location.Y -= 25;
-        gdest.CopyFromScreen(location, new Point(0, 0), size);
-        mainWindow.Invoke(new MethodInvoker(() => mainWindow.ShowMiniScreen(screenPixel)));
-        return screenPixel;
-      }
+   
 
 
-    }
-
-
-    private void HotKeyPressed(object sender, HotkeyEventArgs e)
-    {
-      pause = !pause;
-      if (!pause)
-      {
-        RunBot();
-      }
-      else mainWindow.Invoke(new MethodInvoker(() => mainWindow.ChangeStatusText("Paused", false)));
-      e.Handled = true;
-    }
 
     private void RunBot()
     {
@@ -141,11 +132,9 @@ namespace FishingBot.Methods
         mainWindow.Invoke(new MethodInvoker(() => mainWindow.ChangeStatusText("Analyzing", false)));
 
         GetCursorPos(ref cursor);
-        if (LastAppendedLure == null) {
-          AppendLure();
-          LastAppendedLure = DateTime.Now;
-        }
-        if (DateTime.Now > LastAppendedLure.AddMinutes(lureTimer)) {
+
+        //lure check
+        if (LastAppendedLure == null || DateTime.Now > LastAppendedLure.AddMinutes(lureTimer)) {
           AppendLure();
           LastAppendedLure = DateTime.Now;
         }
@@ -154,29 +143,25 @@ namespace FishingBot.Methods
         Thread.Sleep(1000);
 
 
-        var c = GetPicture(cursor);
-        //var hej = CheckColors(c);
-
-
-       var inPosition = GetInPos(cursor, c);
+        var pic = GetPicture(cursor);
+        var inPosition = GetInPos(cursor, pic);
         
         var FishHooked = false;
         GetCursorPos(ref cursor);
 
-         startPos = get100x100(GetPicture(cursor));
+         startPos = get50x100(GetPicture(cursor));
         int triesLoop = 0;
         while (!FishHooked && inPosition)
         {
           if (pause) break;
           GetCursorPos(ref cursor);
-          c = GetPicture(cursor);
-          var fishwasHooked = CatchFish(c,startPos);
+          pic = GetPicture(cursor);
+          var fishwasHooked = CatchFish(pic,startPos);
           //need to get owning form to display what pixel we see now.
           //if color matches
           if (fishwasHooked)
           {
             mainWindow.Invoke(new MethodInvoker(() => mainWindow.ChangeStatusText(" Fish Found " + DateTime.Now.ToString(), false)));
-            //send left button down
             uint X = (uint)Cursor.Position.X;
             uint Y = (uint)Cursor.Position.Y;
    
@@ -200,36 +185,8 @@ namespace FishingBot.Methods
 
 
     }
-    private void AppendLure() {
-      GetCursorPos(ref cursor);
-      uint X = (uint)Cursor.Position.X;
-      uint Y = (uint)Cursor.Position.Y;
-      SendKeys.SendWait(bind);
-      Thread.Sleep(6000);
-
-      GetWindowRect(h, ref windowRec);
-      var middleY = (windowRec.Bottom - windowRec.Top) / 2;
-      var middleX = (windowRec.Right - windowRec.Left) / 2;
-      cursor.X = middleX;
-      cursor.Y = middleY;
-      SetCursorPos(cursor.X, cursor.Y);
 
 
-      mouse_event(MOUSEEVENTF_RIGHTDOWN, X, Y, 0, 0);
-      Thread.Sleep(50);
-      mouse_event(MOUSEEVENTF_RIGHTUP, X, Y, 0, 0);
-    }
-    private void LogOut() {
-  
-      SendKeys.SendWait("~");
-      SendKeys.SendWait("/");
-      SendKeys.SendWait("C");
-      SendKeys.SendWait("A");
-      SendKeys.SendWait("M");
-      SendKeys.SendWait("P");
-      SendKeys.SendWait("~");
-      pause = true;
-    }
     public bool GetInPos(Point cursor, Bitmap c)
     {
       Rect windowRec = new Rect();
@@ -269,9 +226,10 @@ namespace FishingBot.Methods
       }
       return false;
     }
+
     private bool CatchFish(Bitmap screen,Bitmap startPos)
     {
-      var screensmall = get100x100(screen);
+      var screensmall = get50x100(screen);
       mainWindow.Invoke(new MethodInvoker(() => mainWindow.ShowMiniScreenDebug1(startPos)));
       mainWindow.Invoke(new MethodInvoker(() => mainWindow.ShowMiniScreenDebug2(screensmall)));
       var screenSmallHash = GetHash(screensmall);
@@ -322,8 +280,22 @@ namespace FishingBot.Methods
       }
       return null;
     }
+    private Bitmap GetPicture(Point location)
+    {
+      Size size = new Size(200, 200);
+      Bitmap screenPixel = new Bitmap(200, 200, PixelFormat.Format32bppArgb);
+      using (Graphics gdest = Graphics.FromImage(screenPixel))
+      {
+        location.X -= 75;
+        location.Y -= 25;
+        gdest.CopyFromScreen(location, new Point(0, 0), size);
+        mainWindow.Invoke(new MethodInvoker(() => mainWindow.ShowMiniScreen(screenPixel)));
+        return screenPixel;
+      }
 
-    public Bitmap get100x100(Bitmap src) {
+
+    }
+    private Bitmap get50x100(Bitmap src) {
       Bitmap screenPixel = new Bitmap(50, 100, PixelFormat.Format32bppArgb);
       using (Graphics gdest = Graphics.FromImage(screenPixel))
       {
@@ -331,22 +303,68 @@ namespace FishingBot.Methods
         return screenPixel;
       }
     }
-    public int GetHash(Bitmap bmpSource)
+    private int GetHash(Bitmap bmpSource)
     {
-      int rgbaTotal = 0;
+      int rgbTotal = 0;
       for (int j = 0; j < bmpSource.Height; j++)
       {
         for (int i = 0; i < bmpSource.Width; i++)
         {
           var pixel = bmpSource.GetPixel(i, j);
-          rgbaTotal += pixel.R + pixel.G + pixel.B;
+          rgbTotal += pixel.R + pixel.G + pixel.B;
         }
       }
-      return rgbaTotal;
+      return rgbTotal;
     }
-    public float Compare(int hash1, int hash2) {
+    private float Compare(int hash1, int hash2) {
       return (float) hash1 / hash2;
     }
+
+    private void HotKeyPressed(object sender, HotkeyEventArgs e)
+    {
+      pause = !pause;
+      if (!pause)
+      {
+        RunBot();
+      }
+      else mainWindow.Invoke(new MethodInvoker(() => mainWindow.ChangeStatusText("Paused", false)));
+      e.Handled = true;
+    }
+    
+    private void LogOut()
+    {
+
+      SendKeys.SendWait("~");
+      SendKeys.SendWait("/");
+      SendKeys.SendWait("C");
+      SendKeys.SendWait("A");
+      SendKeys.SendWait("M");
+      SendKeys.SendWait("P");
+      SendKeys.SendWait("~");
+      pause = true;
+    }
+
+    private void AppendLure()
+    {
+      GetCursorPos(ref cursor);
+      uint X = (uint)Cursor.Position.X;
+      uint Y = (uint)Cursor.Position.Y;
+      SendKeys.SendWait(bind);
+      Thread.Sleep(6000);
+
+      GetWindowRect(h, ref windowRec);
+      var middleY = (windowRec.Bottom - windowRec.Top) / 2;
+      var middleX = (windowRec.Right - windowRec.Left) / 2;
+      cursor.X = middleX;
+      cursor.Y = middleY;
+      SetCursorPos(cursor.X, cursor.Y);
+
+
+      mouse_event(MOUSEEVENTF_RIGHTDOWN, X, Y, 0, 0);
+      Thread.Sleep(50);
+      mouse_event(MOUSEEVENTF_RIGHTUP, X, Y, 0, 0);
+    }
+
 
 
 
